8f14e3a5cb883d10d61030a56c97c321403f16dd
nossar <matheusnossar@gmail.com>
2026-08-26 00:24:48 -0300

    Small ruff changes

--- backend/clubi/wsgi.py
+++ backend/clubi/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clubi.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clubi.settings")
 
 application = get_wsgi_application()
